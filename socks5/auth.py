114e6081100f1d25eda1d4f56257a64d0ab22ee4
Matt Snider <matt.snider@alum.utoronto.ca>
2016-10-03 12:45:05 +0200

    Update auth.user_password() to check a credentials dict

--- socks5/auth.py
+++ socks5/auth.py
@@ -1,7 +1,7 @@
 from . import exceptions
-from .log import logger
 
-async def user_password(reader, writer):
+
+async def user_password(reader, writer, credentials):
     _, user_length = await reader.readexactly(2)
     username = await reader.readexactly(int(user_length))
     password_length,  = await reader.readexactly(1)
@@ -10,13 +10,12 @@ async def user_password(reader, writer):
     username = username.decode()
     password = password.decode()
 
-    # TODO: credentials need to come form somewhere
-    success = False
-    if password == 'password':
-        success = True
-        writer.write(b'\x01\x00')
-    else:
-        writer.write(b'\x01\x01')
+    try:
+        success = (password == credentials[username])
+    except KeyError:
+        success = False
+
+    writer.write(b'\x01' + bytes([not success]))
     await writer.drain()
     if not success:
         raise exceptions.AuthFailed("Bad password for '{}'".format(username))
