7582fb89a2d6109730340edc88fda60465956b5b
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-04-28 19:09:13 +0800

    packaging

---
+++ setup.py
@@ -0,0 +1,95 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='socks5server',  # Required
+
+    version='0.1.0',  # Required
+
+    description='A simple socks5 server',  # Required
+
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',  # Optional (see note above)
+
+    url='https://github.com/matt-snider/socks5',  # Optional
+
+    author='matt-snider eternal_flame-AD',  # Optional
+
+    #author_email='a@b.com',  # Optional
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='socks socks5 proxy',  # Optional
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    install_requires=[
+        'click',
+        'kaviar',
+    ],  # Optional
+
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'socks5server=socks5.cli:run_server',
+        ],
+    },
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={
+        'Source': 'https://github.com/matt-snider/socks5',
+    },
+)
