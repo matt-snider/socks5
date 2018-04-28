d205690ee720ce921e8efb87f6de4f01b583ed85
Matt Snider <matt.snider@alum.utoronto.ca>
2018-04-28 14:10:46 +0200

    Update setup.py with proper python version, dependencies, classifiers, etc

--- setup.py
+++ setup.py
@@ -1,78 +1,43 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+#!/usr/bin/env python
+import os
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup, find_packages
+
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md', 'r', 'utf-8') as f:
     long_description = f.read()
 
 
 setup(
-    name='socks5server',  # Required
-
-    version='0.1.0',  # Required
-
-    description='A simple socks5 server',  # Required
-
+    name='socks5server',
+    version='0.1.0',
+    description='A simple asyncio-based socks5 server',
     long_description=long_description,
-
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    url='https://github.com/matt-snider/socks5',  # Optional
-
-    author='matt-snider eternal_flame-AD',  # Optional
-
-    #author_email='a@b.com',  # Optional
-
+    url='https://github.com/matt-snider/socks5',
+    author='matt-snider',
+    author_email='matt.snider@protonmail.com',
     classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
         'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-
-    keywords='socks socks5 proxy',  # Optional
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-
+    keywords='socks socks5 proxy asyncio',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires=">=3.6",
     install_requires=[
         'click',
         'kaviar',
-    ],  # Optional
-
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    entry_points={  # Optional
+    ],
+    entry_points={
         'console_scripts': [
             'socks5server=socks5.cli:run_server',
         ],
     },
-
     project_urls={
         'Source': 'https://github.com/matt-snider/socks5',
     },
